b336c61a36a63b7317bd4c917317aa6b3aff4ec6
Miguel Angel Fuentes Garcia <miguel.fuentes@tergar.org>
2019-10-10 13:02:56 -0500

    Creating a new company

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -2,7 +2,29 @@ module.exports = {
  
     create(req, res){
 
-        return res.ok('OK!');
+     //get the name from the req params
+      let params = req.allParams();
+      if(!params.name){
+          return res.badRequest({err: 'name is required field'});
+      }
+
+      //Create a new record in company 
+      Company.create({
+           name: params.name,
+           city: params.city,
+           address: params.address
+      }, (err, results)=>{
+
+         if(err){
+             return res.serverError(err);
+         }
+         return res.ok('OK!');
+
+      });
+
+
+
+        
     },
 
     find(req, res){
